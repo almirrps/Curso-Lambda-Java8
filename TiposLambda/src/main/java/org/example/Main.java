4f0d17250213e642e53ccd1c359394b298809a14
Almir <almirrps@hotmail.com>
2023-05-08 06:10:11 -0300

    Formas de escrever um lambda

---
+++ TiposLambda/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // *** Parenteses *** \\
+        Runnable runnable = () -> System.out.println("Vendo tipos de lambda.");  // Quando não temos parâmetros no método que chamamos
+
+        IntStream.range(0, 5)
+                .filter((int n) -> n % 2 == 0)  // Quando queremos declarar o tipo da variável
+                .reduce((n1, n2) -> n1 + n2)    // Quando temos mais de um parâmetro para passar
+                .ifPresent(System.out::println);
+
+        // *** Chaves *** \\
+        IntStream.range(0, 5)
+                .filter(n -> {  // Quando queremos executar mais de um método - lembre-se do ponto e vírgula
+                    System.out.println("Exemplo de parenteses");
+                    return n % 2 == 0;
+                })
+                .forEach(n -> System.out.println(n));  // Simples
+    }
+}
